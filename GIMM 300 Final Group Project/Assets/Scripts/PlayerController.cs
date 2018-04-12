47256830d14b429284aaec1281f4a96ef7b558cb
Mario Townes <35517379+NovasonicGBAstyle@users.noreply.github.com>
2018-04-12 00:10:21 -0600

    First Check In
    So, I got this to run on mobile to begin with.  For some reason the movement isn't working.  Instead of returning the value, it's returning nothing.  It's really pissing me off.  I even made the value static in case it was somehow creating two classes and the class being called wasn't the class making the changes.  Gave it like an hour, and I just can't keep going.  I'll have to just continue and figure it out later hopefully.

--- GIMM 300 Final Group Project/Assets/Scripts/PlayerController.cs
+++ GIMM 300 Final Group Project/Assets/Scripts/PlayerController.cs
@@ -12,12 +12,29 @@ public class PlayerController : MonoBehaviour {
     public float fireRate = 0.5f;
     private float nextFire = 0.0f;
 
+    public SimpleTouchPad touchPad;
+
+    private Quaternion calibrationQuaternion;
+
+    private void Start()
+    {
+        CalibrateAccelerometer();
+    }
+
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //Old stuff for just computer.
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        //This gets input from the accelerometer.
+        //Vector3 accelerationRaw = Input.acceleration;
+        //Vector3 acceleration = FixAcceleration(accelerationRaw);
+        //Vector3 movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
+
+        Vector2 direction = touchPad.GetDirection();
+        Vector3 movement = new Vector3(direction.x, 0.0f, direction.y);
 
         GetComponent<Rigidbody>().velocity = movement * speed;
 
@@ -38,6 +55,21 @@ public class PlayerController : MonoBehaviour {
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
             GetComponent<AudioSource>().Play();
         }
+    }
+
+    //Used toe calibrate the Input.acceleration input.
+    void CalibrateAccelerometer()
+    {
+        Vector3 accelerationSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+
+    //Get the 'calibrated' value from the Input
+    Vector3 FixAcceleration(Vector3 acceleration)
+    {
+        Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+        return fixedAcceleration;
     }
 }
 
---
+++ GIMM 300 Final Group Project/Assets/Scripts/SimpleTouchPad.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleTouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
+    private Vector2 origin;
+    private static Vector2 direction;
+    
+    void Awake()
+    {
+        //direction = Vector2.zero;
+    }
+
+    public void OnPointerDown (PointerEventData data)
+    {
+        //Set our start point.
+        origin = data.position;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        //Reset everyting.
+        //direction = Vector2.zero;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        //Compare the difference between our start point and our current pointer position
+        Vector2 currentPosition = data.position;
+        Vector2 directionRaw = currentPosition - origin;
+
+        //Again, this will set the value to 1, but keep the direction.
+        Vector2 direction = directionRaw.normalized;
+        Debug.Log(direction);
+    }
+
+    public Vector2 GetDirection()
+    {
+        return direction;
+    }
+}
